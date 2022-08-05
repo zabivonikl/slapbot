aa93888df5beaa1d829c389e4a852f73a3e479a9
textu <texturich@gmail.com>
2022-08-05 11:07:56 +0300

    refactoring in JoinTheGameHandler.cs

--- BotLogic/ChainResponsibilityLinks/JoinTheGameHandler.cs
+++ BotLogic/ChainResponsibilityLinks/JoinTheGameHandler.cs
@@ -9,26 +9,24 @@ namespace BotLogic.ChainResponsibilityLinks;
 
 public class CreateGameHandler : AbstractHandler
 {
-    private Update handleableUpdate = null!;
+    private Update update = null!;
 
     public CreateGameHandler(KeyboardFactory keyboardFactory, AbstractHandler? next = null) : base(keyboardFactory, next)
     {
     }
 
-    protected override bool CanHandle(Update update) => update.Message == "Вступить в игру";
+    protected override bool CanHandle(Update incomingUpdate) => incomingUpdate.Message == "Вступить в игру";
 
-    protected override async Task _Handle(Update update)
+    protected override async Task _Handle(Update incomingUpdate)
     {
+        update = incomingUpdate;
         await base._Handle(update);
-        handleableUpdate = update;
         await using var context = new SlapBotDal();
         try
         {
-            if (context.Users.All(u => u.Id != handleableUpdate.User.Id))
-                await CreateUser(context, handleableUpdate.User.GetAdapter());
-            else if (context.Games.Include(g => g.Users).Any(
-                             g => g.Id == handleableUpdate.Chat.Id && g.Users.Any(u => u.Id == handleableUpdate.User.Id)
-                         ))
+            if (!UserExist(context))
+                await CreateUser(context, update.User.GetAdapter());
+            else if (UserInCurrentGame(context))
                 throw new InvalidOperationException("User already in this game");
             await TryCreateGame(context);
             await FindGameAndAddUser(context);
@@ -44,24 +42,31 @@ public class CreateGameHandler : AbstractHandler
         }
         catch (InvalidOperationException ex1) when (ex1.Message == "User already in this game")
         {
-            await handleableUpdate.Messenger.SendMessage(
-                    handleableUpdate.Chat,
+            await update.Messenger.SendMessage(
+                    update.Chat,
                     "Вы уже участвуете.",
                     keyboardFactory.GetStartKeyboard()
                 );
         }
     }
 
+    private bool UserExist(SlapBotDal context) => context.Users.Any(u => u.Id == update.User.Id);
+
+    private bool UserInCurrentGame(SlapBotDal context) =>
+        context.Games
+            .Include(g => g.Users)
+            .Any(g => g.Id == update.Chat.Id && g.Users.Any(u => u.Id == update.User.Id));
+
     private async Task TryCreateGame(SlapBotDal context)
     {
-        if (context.Games.Any(g => g.Id == handleableUpdate.Chat.Id))
+        if (context.Games.Any(g => g.Id == update.Chat.Id))
             throw new InvalidOperationException("Game already exist");
         await CreateGame(context);
     }
 
     private async Task CreateGame(SlapBotDal context)
     {
-        await context.Games.AddAsync(new Game(handleableUpdate.Chat.Id));
+        await context.Games.AddAsync(new Game(update.Chat.Id));
         await context.SaveChangesAsync();
     }
 
@@ -69,8 +74,8 @@ public class CreateGameHandler : AbstractHandler
     {
         var game = await context.Games
             .Include(g => g.Users)
-            .SingleAsync(g => g.Id == handleableUpdate.Chat.Id);
-        game.Users.Add(handleableUpdate.User.GetAdapter());
+            .SingleAsync(g => g.Id == update.Chat.Id);
+        game.Users.Add(update.User.GetAdapter());
         await context.SaveChangesAsync();
     }
 
@@ -83,8 +88,8 @@ public class CreateGameHandler : AbstractHandler
     private async Task JoinToGame(SlapBotDal context)
     {
         await FindGameAndAddUser(context);
-        await handleableUpdate.Messenger.SendMessage(
-                handleableUpdate.Chat,
+        await update.Messenger.SendMessage(
+                update.Chat,
                 "Вы вступили в игру",
                 keyboardFactory.GetStartKeyboard()
             );
